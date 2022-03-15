68817cc24019a753fd653d587cb0079e163ed70c
Daniil <daniilafendulov@bk.ru>
2022-03-15 19:34:27 +0300

    Remove ActivationFunctions namespace

--- NeuronNetworkLibrary/ActivationFunctions/LinearFunction.cs
+++ NeuronNetworkLibrary/ActivationFunctions/LinearFunction.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NeuronNetworkProject.ActivationFunctions
+namespace NeuronNetworkProject
 {
     public class LinearFunction : IActivationFunction
     {
